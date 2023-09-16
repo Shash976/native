25fc20a4cdda6edf90366ea9286388cf84698d86
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2023-09-16 10:47:36 +0530

    Added react-native-ble-manager

--- arduino-hc05-app/App.js
+++ arduino-hc05-app/App.js
@@ -1,15 +1,63 @@
-//import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, FlatList } from 'react-native';
+import BleManager from 'react-native-ble-manager';
 
-export default function App() {
+const App = () => {
+  const [isScanning, setIsScanning] = useState(false);
+  const [devices, setDevices] = useState([]);
+  const [connectedDevice, setConnectedDevice] = useState(null);
+  const [receivedData, setReceivedData] = useState(null);
+
+  useEffect(() => {
+    BleManager.start({ showAlert: false });
+  }, []);
+
+  const startScan = () => {
+    setIsScanning(true);
+    BleManager.scan([], 5, true)
+      .then(() => {
+        console.log('Scanning...');
+      });
+  };
+
+  const stopScan = () => {
+    setIsScanning(false);
+    BleManager.stopScan()
+      .then(() => {
+        console.log('Scan stopped');
+      });
+  };
+
+  const connectToDevice = (device) => {
+    BleManager.connect(device.id)
+      .then(() => {
+        console.log('Connected to ' + device.id);
+        setConnectedDevice(device);
+      })
+      .catch((error) => {
+        console.log('Connection failed', error);
+      });
+  };
+
+  // Add your logic to perform operations based on receivedData
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>HC05 Bluetooth Communication</Text>
+      <Button title={isScanning ? 'Stop Scan' : 'Start Scan'} onPress={isScanning ? stopScan : startScan} />
+      <FlatList
+        data={devices}
+        renderItem={({ item }) => (
+          <Button title={item.name || item.id} onPress={() => connectToDevice(item)} />
+        )}
+        keyExtractor={(item) => item.id}
+      />
+      <Text>Received Data: {receivedData}</Text>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
